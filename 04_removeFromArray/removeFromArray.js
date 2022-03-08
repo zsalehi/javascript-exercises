19e1fccb63912f4a6d971ec0a923bc86a5ed328b
Zaigham Salehi <zsalehi313@gmail.com>
2022-03-08 12:21:04 -0500

    Complete js exercises 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
 
+// advanced form - essentially what I should do is first understand
+// that all items passed as arguments can be arrays within arrays
+// therefore by pulling out the first item from the args array,
+// i pull out the initial array being passed.
+// to pass all the tests there are 2 options to go from
+// 1. i can use a forEach on the newArray i would create, and push items that are NOT in the functions argument array
+// 2. use a .filter method to filter out arguments NOT in the functions argument array 
+const removeFromArray = function(...args) {
+    const array = args[0];
+    return array.filter(val => !args.includes(val));
+
+    // option 1
+    // const newArray = [];
+    // array.forEach((items) => {
+    //    if(!args.includes(item)) {
+    //        newArray.push(item);
+    //    }
+    //}
+    // return newArray;
 };
 
 // Do not edit below this line
