5b963ddc5e67f122e74ea08a49e458bc9cf93315
Zaigham Salehi <zsalehi313@gmail.com>
2022-03-08 16:23:34 -0500

    Complete js exercises 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let celsius = (tempF - 32) * 5/9;
+  celsius = Math.round(celsius *10)/10
+  return celsius;
 };
 
-const ctof = function() {
+const ctof = function(tempC) {
+  let fahrenheit = (tempC * 9/5) + 32;
+  fahrenheit = Math.round(fahrenheit *10)/10
+  
+  return fahrenheit;
+
+
+
 
 };
 
