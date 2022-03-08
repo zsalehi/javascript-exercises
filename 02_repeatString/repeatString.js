4748b6b981ef3fdc24806ab6d92f85786697e9cc
Zaigham Salehi <zsalehi313@gmail.com>
2022-03-08 10:58:33 -0500

    Complete js exercises 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) return 'ERROR';
+    let string = "";
+    for (let i = 0; i < num; i++) {
+        string += str;
+    }
+    return string;
 };
 
 // Do not edit below this line
