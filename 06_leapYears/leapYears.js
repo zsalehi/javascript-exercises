ded9946b3f1b52949550a6574ceaf852205cb997
Zaigham Salehi <zsalehi313@gmail.com>
2022-03-08 16:05:20 -0500

    Complete js exercises 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let lYear;
+
+    if (year % 4 === 0 && !(year % 100 === 0)) {
+        lYear = true;
+    } else if (year % 100 === 0 && year % 400 === 0) {
+        lYear = true;
+    } else {
+        lYear = false;
+    }
+
+    return lYear;
 
 };
 
+
 // Do not edit below this line
 module.exports = leapYears;
+
+
+// if (year) % 4 === 0  => true
+// if year % 100 === 0 => false
+// if year % 400 === 0 && year % 100 === 0 => true
+// if (year) % 4 && !(year) % 100 NOT A LEAP YEAR
+// if (year) % 400 
