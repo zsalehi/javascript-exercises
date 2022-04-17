d7e8bb775b03c3328b28ab327956c641ef5b44ac
Zaigham Salehi <zsalehi313@gmail.com>
2022-04-17 16:09:38 -0400

    Completed all functions in calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,20 +10,38 @@ const subtract = function(...args) {
   })
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sumOfNumbers = 0;
+  for(var i = 0; i < arr.length; i++){
+    sumOfNumbers += arr[i];
+  }
+  return sumOfNumbers;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(function(acc, cur){
+    return acc * cur;
+  })
 };
 
-const power = function() {
-	
+const power = function(...args) {
+	return args.reduce(function (acc, cur){
+    return acc ** cur;
+  })
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	// If the number is less than 0 return -1 since no factorials can be done.
+  if (num < 0)
+    return -1;
+  // If the number is 0, its factorial is 1.
+  else if (num === 0)
+    return 1;
+  else {
+    // This is a recursive function, and so it calls itself again and again until the num-1 becomes 1 
+    return (num * factorial(num-1));
+
+  }
 };
 
 // Do not edit below this line
