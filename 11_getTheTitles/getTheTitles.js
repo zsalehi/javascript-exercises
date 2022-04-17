ce419f002a646f5173cc9c583df58a318b3facb3
Zaigham Salehi <zsalehi313@gmail.com>
2022-04-17 17:13:43 -0400

    Complete get the Titles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const bookTitles = books.map( (book) => `${book.title}`);
+    return bookTitles;
 };
 
 // Do not edit below this line
