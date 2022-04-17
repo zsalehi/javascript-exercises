2709e34d82af3187d61e1105e0ddc7b222ed5079
Zaigham Salehi <zsalehi313@gmail.com>
2022-04-17 16:25:50 -0400

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    var re = /[\W_]/g;
+    
+    // 1. Remove all punctuation and make the string lowercase.
+
+    let lowRegStr = str.toLowerCase().replace(re, '');
+    
+    // 2. Use chained methods to create a reverse string and join it together. Turns into an array, reverses order, then joins together.
+    let reverseStr = lowRegStr.split('').reverse().join('');
+
+    // 3. Check if the reverseStr equals the lowRegStr.
+
+    return reverseStr === lowRegStr;
 
 };
 
